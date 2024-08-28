fb1fccdeb736c96d6d015b227f035fc172178f7b
강은찬 <kangeunchan080310@gmail.com>
2024-08-28 19:19:31 +0900

    feat : 프로젝트 설정

---
+++ src/main/java/com/teamclim/climapiservice/ClimApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.teamclim.climapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimApiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClimApiServiceApplication.class, args);
+    }
+
+}
